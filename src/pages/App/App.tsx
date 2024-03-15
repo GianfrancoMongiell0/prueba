f2afd46e040cdcfb2f840d4bd787167861364962
GianfrancoMongiell0 <gianfrnaco.mongiello@correo.unimet.edu.ve>
2024-03-15 01:45:40 -0400

    subida

---
+++ src/pages/App/App.tsx
@@ -0,0 +1,11 @@
+import NavBar from "../NavBar/NavBar"
+
+export default function App() {
+    return (
+        <>
+            <NavBar></ NavBar>
+
+            <div className="texto">hola</div>
+        </>
+    )
+}
---
+++ src/pages/NavBar/NavBar.tsx
@@ -0,0 +1,15 @@
+import '../Styles/NavBar.css'
+
+export default function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="servicios.html">Servicios</a></li>
+                <li><a href="nosotros.html">Nosotros</a></li>
+                <li><a href="contacto.html">Contacto</a></li>
+            </ul>
+        </nav>
+
+    )
+}
