a7245bb332ee054fa6e14ddf221825aa94ccac96
GianfrancoMongiell0 <gianfrnaco.mongiello@correo.unimet.edu.ve>
2024-03-15 23:25:10 -0400

    ultimos arreglos

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,52 +1,60 @@
 
 import styled from 'styled-components';
 import { useState } from 'react';
-import BurgerButton from '../../components/BurgerButton';
+import BurgerButton from './BurgerButton';
 
 export default function NavBar() {
 
-    const [active, setActive] = useState(false)
-    const handleClick = () => {
-        setActive(!active)
-    }
+  const [active, setActive] = useState(false)
+  const handleClick = () => {
+    setActive(!active)
+  }
 
 
-    return (
-        <>
-            <NavBarContainer>
-                <h2>Navbar <span>responsive</span></h2>
-                <div className={`links ${active ? 'active' : ''}`}>
-                    <a href="" >Sobre mi</a>
-                    <a href="">Contacto</a>
-                    <a href="">Portafolio</a>
-                    <a href="">Conocimientos</a>
-                </div>
-                <div className="burguer">
-                    <BurgerButton active={active} handleClick={handleClick} />
-                </div>
-                <BgDiv className={`initial ${active ? 'active' : ''}`}></BgDiv>
-            </NavBarContainer>
-        </>
-    );
+  return (
+    <>
+      <NavBarContainer>
+        <h2>Navbar <span>responsive</span></h2>
+        <div className={`links ${active ? 'active' : ''}`}>
+          <a href="" className='link'>Sobre mi</a>
+          <a href="" className='link'>Contacto</a>
+          <a href="" className='link'>Portafolio</a>
+          <a href="" className='link'>Conocimientos</a>
+        </div>
+        <div className="burguer">
+          <BurgerButton active={active} handleClick={handleClick} />
+        </div>
+        <BgDiv className={`initial ${active ? 'active' : ''}`}></BgDiv>
+      </NavBarContainer>
+    </>
+  );
 }
 
 const NavBarContainer = styled.div`
 h2{
+  font-size: 2rem;
     color: white;
     font-weight: 400;
+
     span{
       font-weight: bold;
+      color: #f1c40f;
+      font-size: 1.5rem;
     }
   }
+
   padding: .4rem;
   background-color: #333;
   display: flex;
   align-items: center;
   justify-content: space-between;
+
   a{
-    color: white;
+    color: black;
     text-decoration: none;
     margin-right: 1rem;
+    display: block;
+    padding: 1rem;
   }
   .links{
     position: absolute;
@@ -57,22 +65,22 @@ h2{
     margin-right: auto;
     text-align: center;
     transition: all .5s ease;
-    a{
-      color: white;
-      font-size: 2rem;
-      display: block;
-    }
-    @media(min-width: 768px){
+    
+    @media(min-width: 1024px){
       position: initial;
       margin: 0;
       a{
-        font-size: 1rem;
+        font-size: 1.5rem;
         color: white;
         display: inline;
+      }
+      a:hover{
+        color: #f1c40f;
       }
       display: block;
     }
   }
+
   .links.active{
     width: 100%;
     display: block;
@@ -86,18 +94,22 @@ h2{
     a{
       font-size: 2rem;
       margin-top: 1rem;
-      color: white;
+      color: #white;
     }
   }
+
+  a:hover{color: #f1c40f;}
+
+  
   .burguer{
-    @media(min-width: 768px){
+    @media(min-width: 1024px){
       display: none;
     }
   }
 `
 
 const BgDiv = styled.div`
-  background-color: #222;
+  background-color: #ffffff;
   position: absolute;
   top: -1000px;
   left: -1000px;
